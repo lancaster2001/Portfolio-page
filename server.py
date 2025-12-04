32caa089edb0d48b0f96c23bb5cc38b519e26682
Alex Lancaster <alextwickenham2001@hotmail.com>
2025-12-04 17:44:32 +0000

    updated python script to allows for multithreading to present the server from crashing when to many requests come in

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from http.server import SimpleHTTPRequestHandler, HTTPServer
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
 
 class NoListing(SimpleHTTPRequestHandler):
     def list_directory(self, path):
@@ -6,6 +6,6 @@ class NoListing(SimpleHTTPRequestHandler):
         return None
 
 if __name__ == "__main__":
-    server = HTTPServer(("0.0.0.0", 3002), NoListing)
+    server = ThreadingHTTPServer(("0.0.0.0", 3002), NoListing)
     print("Serving from working directory...")
     server.serve_forever()
