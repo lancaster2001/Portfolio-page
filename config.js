27c12bef12888dfd6a0eebda29a94b5bbb7b364b
Alex Lancaster <alextwickenham2001@hotmail.com>
2026-02-28 16:07:03 +0000

    - ensured view port is declared on every page - added a global config - implemented global config into all pages 	- added "loading.." to variables that use the config script 	- moved footer to config script 	- moved header 1 to config script 	- moved job title to config script

---
+++ config.js
@@ -0,0 +1,25 @@
+//Global variables for site configuration
+//Note: when adding new variables, add them to the setById function below for use on the page
+const SITE = {
+  name: "Alex Lancaster",
+  job_title: "Technical Consultant",
+  github: "https://github.com/Lancaster2001",
+  linkedin: "https://www.linkedin.com/in/alex-lancaster-7437671b4/",
+}
+
+//Sets page Title
+document.title = SITE.name + " | Portfolio";
+
+//Error handles variables that aren't used
+function setById(id, prop, value) {
+  const element = document.getElementById(id);
+  if (element) element[prop] = value;
+}
+
+//Set variables for page use
+setById("year", "textContent", new Date().getFullYear());
+setById("name", "textContent", SITE.name);
+setById("job_title", "textContent", SITE.job_title);
+setById("footer", "textContent",  `Built and maintained by ${SITE.name} — © ${new Date().getFullYear()}`);
+setById("github_link", "href", SITE.github);
+setById("linkedin_link", "href", SITE.linkedin);
